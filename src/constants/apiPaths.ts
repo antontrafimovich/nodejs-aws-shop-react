a47047c4db64085f7090987ed4bc292e75b4075a
Anton Trafimovich <tonytrof@gmail.com>
2023-06-18 20:08:30 +0200

    feat: add support for import functionality.

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://zpyu7ybhs2.execute-api.eu-north-1.amazonaws.com",
   bff: "https://z21ypzzft3.execute-api.eu-north-1.amazonaws.com",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
