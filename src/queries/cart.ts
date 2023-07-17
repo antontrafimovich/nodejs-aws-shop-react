17349f369886dcc9b3e146b7ccfc75585cf8a066
Anton Trafimovich <tonytrof@gmail.com>
2023-07-18 00:56:24 +0200

    feat: add correct response handler for cart GET.

--- src/queries/cart.ts
+++ src/queries/cart.ts
@@ -4,14 +4,28 @@ import { useQuery, useQueryClient, useMutation } from "react-query";
 import API_PATHS from "~/constants/apiPaths";
 import { CartItem } from "~/models/CartItem";
 
+type CartResponse = {
+  data: {
+    message: string;
+    statusCode: number;
+    cart: {
+      id: string;
+      items: CartItem[];
+    };
+  };
+};
+
 export function useCart() {
   return useQuery<CartItem[], AxiosError>("cart", async () => {
-    const res = await axios.get<CartItem[]>(`${API_PATHS.cart}/profile/cart`, {
-      headers: {
-        Authorization: `Basic ${localStorage.getItem("authorization_token")}`,
-      },
-    });
-    return res.data;
+    const res = await axios.get<CartResponse>(
+      `${API_PATHS.cart}/profile/cart`,
+      {
+        headers: {
+          Authorization: `Basic ${localStorage.getItem("authorization_token")}`,
+        },
+      }
+    );
+    return res.data.data.cart.items;
   });
 }
 
