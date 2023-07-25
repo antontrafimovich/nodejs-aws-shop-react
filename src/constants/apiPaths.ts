74fe87539878533c14a2b83a26f31565e4a0fca9
Anton Trafimovich <tonytrof@gmail.com>
2023-07-25 23:25:28 +0200

    fix: use correct api links.

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "http://antontrafimovich-cart-api-develop.us-east-1.elasticbeanstalk.com/import",
-  bff: "http://antontrafimovich-cart-api-develop.us-east-1.elasticbeanstalk.com/products",
-  cart: "http://antontrafimovich-cart-api-develop.us-east-1.elasticbeanstalk.com/cart/api",
+  import: "http://antontrafimovich-bff-api-dev.us-east-1.elasticbeanstalk.com/import",
+  bff: "http://antontrafimovich-bff-api-dev.us-east-1.elasticbeanstalk.com/products",
+  cart: "http://antontrafimovich-bff-api-dev.us-east-1.elasticbeanstalk.com/cart/api",
 };
 
 export default API_PATHS;
